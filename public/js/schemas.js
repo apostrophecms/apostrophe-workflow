eef0abab654353f6c9230340038bce9061a82841
Tom Boutell <tom@punkave.com>
2017-05-10 11:09:05 -0400

    disable interaction with live preview fields

--- public/js/schemas.js
+++ public/js/schemas.js
@@ -27,7 +27,7 @@ apos.define('apostrophe-schemas', {
           // A pristine copy for revert purposes
           var $livePristine = $draft.clone();
           // Nest the twin in an apos-field so that findFieldset for the original schema doesn't mistakenly descend into it
-          var $wrapper = $('<div class="apos-field apos-workflow-live-field" data-name="workflow-live-' + field.name + '"></div>');
+          var $wrapper = $('<div class="apos-field apos-workflow-live-field" data-apos-workflow-live-field data-name="workflow-live-' + field.name + '"></div>');
           $wrapper.append($live);
           $draft.after($wrapper);
           if (!_.isEqual(object[field.name], data.doc[field.name])) {
@@ -100,6 +100,33 @@ apos.define('apostrophe-schemas', {
       function next() {
         return superBeforePopulate($el, schema, object, callback);
       }
+    };
+    var superAfterPopulate = self.afterPopulate;
+    // Disable interaction with the content of a "live" (not draft) field, which is serving as a preview only. Since we
+    // don't control the field populators, it's not perfect, but we disable everything that can be disabled, kill
+    // mouse events, reject focus as much as we can and add a little opacity.
+    self.afterPopulate = function($el, schema, object, callback) {
+      // Make sure people can't edit the preview of the live version of the field
+      $el.find('[data-apos-workflow-live-field] *').each(function() {
+        if (typeof this.disabled != "undefined") {
+          this.disabled = true;
+        }
+      });
+      // But allow the toggles to be clicked
+      $el.find('[data-apos-workflow-live-field] label:first, [data-apos-workflow-live-field] label:first *').each(function() {
+        if (typeof this.disabled != "undefined") {
+          this.disabled = false;
+        }
+      });
+      // Try to give away the focus if it arrives via keyboard on an anchor element
+      $el.on('focus', '[data-apos-workflow-live-field] a', function() {
+        if ($(this).closest('label').length) {
+          return;
+        }
+        $(this).closest('.apos-field').parent().next().find('a,input,select').first().focus();
+        return false;
+      });
+      return superAfterPopulate($el, schema, object, callback);
     };
   }
 });
