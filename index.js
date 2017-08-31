3093f24a81dd4d486410cf4c2a8d507b9830a06c
Tom Boutell <tom@punkave.com>
2017-08-31 15:37:37 -0400

    more progress on cross domain login

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ module.exports = {
     self.addToAdminBar();
     self.apos.pages.addAfterContextMenu(self.menu);
     self.enableHelpers();
+    self.enablecrossDomainSessionCache();
     return self.enableCollection(callback);
   },
 
