ed9d59b529192200b57fdce575891d558cf72151
starsinmypockets <starsinmypockets@gmail.com>
2018-04-25 17:28:28 -0400

    Checkpoint. Revert works, tests prove.

--- test/testApi.js
+++ test/testApi.js
@@ -1,12 +1,13 @@
 var assert = require('assert');
 var async = require('async');
+var revertId;
 debugger;
 
 describe('Workflow Core', function() {
   this.timeout(5000);
   
   after(function() {
-//    apos.db.dropDatabase();
+    apos.db.dropDatabase();
   });
 
   it('should be a property of the apos object', function(done) {
@@ -59,7 +60,8 @@ describe('Workflow Core', function() {
     const req = apos.tasks.getReq();
     return apos.products.insert(req, {
       title: 'initial title',
-      published: true
+      published: true,
+      trash: false
     })
       .then(doc => {
         console.log("DOC", doc)
@@ -68,11 +70,17 @@ describe('Workflow Core', function() {
         assert(doc.workflowLocale === 'default-draft');
       });
   });
-
+  
+  // block repeats
   it('Commmit a change', (done) => {
     var req = apos.tasks.getReq({locale: 'default-draft'});
+    var msgs = ['first commit', 'second commit'];
+    
+    async.waterfall([getProductDraft, updateProductDraft, commitUpdate], (err, res) => {
+      if (err) {
+        console.log('test Commit err', err);
+      }
 
-    async.waterfall([getProductDraft, commitUpdate], (err, res) => {
       assert(!err);
       assert(typeof res === 'string', 'response should be an id');
       done();
@@ -96,9 +104,8 @@ describe('Workflow Core', function() {
     }
 
     function commitUpdate(product, cb) {
-      console.log("PROD", product)
       apos.workflow.commitLatest(req, product._id, (err, res) => {
-        console.log('workflow commit success',err, res);
+        revertId = res;
         return cb(err, res);
       });
     }
@@ -107,10 +114,88 @@ describe('Workflow Core', function() {
   it('Check for live document after commit', done => {
     const req = apos.tasks.getReq();
     apos.products.find(req).toArray().then( docs => {
-       console.log("FOUND", docs);
        assert(docs[0].title === 'new title');
        assert(!docs[0].trash)
        done();
     });
+  });
+  // end block repeats
+
+  // block repeats
+  it('Commmit a change', (done) => {
+    var req = apos.tasks.getReq({locale: 'default-draft'});
+    var msgs = ['first commit', 'second commit'];
+    
+    async.waterfall([getProductDraft, updateProductDraft, commitUpdate], (err, res) => {
+      if (err) {
+        console.log('test Commit err', err);
+      }
+
+      assert(!err);
+      assert(typeof res === 'string', 'response should be an id');
+      done();
+    })
+    
+    function getProductDraft(cb) {
+      apos.products.find(req).toArray().then( docs => {
+        assert(docs[0]);
+        return cb(null, docs[0]);
+      })
+      .catch(e => {
+        return cb(e)
+      });
+    }
+
+    function updateProductDraft(product, cb) {
+      product.title = 'new title 2';
+      apos.products.update(req, product, (err, res) => {
+        return cb(err, res);
+      });
+    }
+
+    function commitUpdate(product, cb) {
+      apos.workflow.commitLatest(req, product._id, (err, res) => {
+        return cb(err, res);
+      });
+    }
+    // end block repeats
+  });
+
+  it('Check for live document after commit', done => {
+    const req = apos.tasks.getReq();
+    apos.products.find(req).toArray().then( docs => {
+       assert(docs[0].title === 'new title 2');
+       assert(!docs[0].trash)
+       done();
+    });
+  });
+
+  it('Test revert', done => {
+    const req = apos.tasks.getReq();
+    assert(revertId);
+    
+    async.waterfall([revert, check], (err, docs) => {
+      assert(!err);
+      assert(docs[0].title === 'new title');
+      assert(!docs[0].trash);
+      done();
+    });
+
+    function revert (cb) {
+      apos.workflow.revert(req, revertId, (err, res) => {
+        if (err) {
+          console.log("Err at API revert");
+        }
+        assert(!err);
+        cb(err);
+      });
+    }
+    
+    function check (cb) {
+      var req = apos.tasks.getReq({locale: 'default-draft'});
+      apos.products.find(req).toArray().then( docs => {
+        cb(null, docs);
+      }).catch(cb)
+    }
   });
 });
