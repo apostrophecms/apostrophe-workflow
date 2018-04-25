9677589fbaab11f7b8ed7455b48e68bf853b9950
starsinmypockets <starsinmypockets@gmail.com>
2018-04-25 11:04:17 -0400

    Test setup for workflow revert

--- test/testApi.js
+++ test/testApi.js
@@ -1,21 +1,9 @@
 var assert = require('assert');
+var api = require('../lib/api');
+var async = require('async');
 var _ = require('lodash');
-
-describe('Workflow Add Missing Locales Inheritance', function() {
-
-  var apos;
-
-  var existsIn = [ 'default', 'us' ];
-
-  this.timeout(5000);
-
-  after(function() {
-    apos.db.dropDatabase();
-  });
-
-  /// ///
-  // EXISTENCE
-  /// ///
+var testDocIds = [];
+var pageInitial;
 
   it('should be a property of the apos object', function(done) {
     apos = require('apostrophe')({
@@ -36,101 +24,41 @@ describe('Workflow Add Missing Locales Inheritance', function() {
           ]
         },
         'apostrophe-workflow': {
-          prefixes: {
-            // Even private locales must be distinguishable by hostname and/or prefix
-            'default': '/default',
-            'us': '/us',
-            'us-en': '/en',
-            'us-es': '/es'
-            // We don't need prefixes for fr because
-            // that hostname is not shared with other
-            // locales
-          },
-          locales: [
-            {
-              name: 'default',
-              label: 'Default',
-              private: true,
-              children: [
-                {
-                  name: 'fr'
-                },
-                {
-                  name: 'us',
-                  private: true,
-                  children: [
-                    {
-                      name: 'us-en'
-                    },
-                    {
-                      name: 'us-es'
-                    }
-                  ]
-                }
-              ]
-            }
-          ],
-          defaultLocale: 'default'
+          settings: {
+            locales: ['default'],
+            defaultLocale: 'default',
+            alias: 'worflow' // for testing only!
+          }
+        },
+        'products': {
+          extend: 'apostrophe-pieces',
+          name: 'product',
+          alias: 'products'
         }
       },
       afterInit: function(callback) {
         assert(apos.modules['apostrophe-workflow']);
-        // Should NOT have an alias!
-        assert(!apos.workflow);
         return callback(null);
       },
       afterListen: function(err) {
+        if (err) {
+          console.log("Existrs", err);
+        }
+
         assert(!err);
         done();
       }
     });
   });
 
-  it('can insert docs at a low level for test purposes', function() {
-    existsIn = existsIn.concat(_.map(existsIn, function(locale) {
-      return locale + '-draft';
-    }));
-    return apos.docs.db.insert(_.map(existsIn, function(locale) {
-      return {
-        workflowGuid: 'abc',
-        workflowLocale: locale,
-        type: 'testPage',
-        origin: locale,
-        title: 'test',
-        slug: '/' + locale + '/test',
-        path: '/' + locale + '/test'
-      };
-    }));
-  });
-
-  it('can execute add-missing-locales task', function(callback) {
-    return apos.modules['apostrophe-workflow'].addMissingLocalesTask(apos, apos.argv, callback);
-  });
-
-  it('missing locales now have copies inherited from correct ancestors', function() {
-    return apos.docs.db.find({ title: 'test' }).toArray().then(function(docs) {
-      assert(docs);
-      assert(docs.length === 10);
-      var fr = _.find(docs, { workflowLocale: 'fr' });
-      assert(fr);
-      assert(fr.origin === 'default');
-      var usEn = _.find(docs, { workflowLocale: 'us-en' });
-      assert(usEn);
-      assert(usEn.origin === 'us');
-    });
-  });
-
-  it('Add draft of document for testing', () => {
-    apos.docs.db.find({ title: 'test', workflowLocale: 'us-en' }).toArray().then(function(docs) {
-      console.log('DD', docs[0]);
-      const draft = Object.assign(docs[0], {
-        "body" : {
-           "items" : [ ],
-           "type" : "area"
-        }        
+  it('Test add draft product to db as draft', () => {
+    var req = apos.tasks.getReq({locale: 'default-draft'});
+    return apos.products.insert(req, {title: 'initial title'})
+      .then(doc => {
+        console.log("DOC", doc)
+        assert(doc.type === 'product');
+        assert(doc.workflowGuid);
+        assert(doc.workflowLocale === 'default-draft');
       });
-      console.log("DD", draft);
-		  apos.docs.db.update({_id: draft._d}, draft);	
-    });
   });
-});
+
