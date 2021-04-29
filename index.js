fa885ade295c8d554dc588cdf0f415bb447d42d4
Jed <valentin.jeudy@gmail.com>
2021-04-29 19:51:45 +0200

    adds migration

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ module.exports = {
     self.enableHarmonizeWorkflowGuidsByParkedIdTask();
     self.enableDiffDraftAndLiveTask();
     self.enableReplicateLocaleTask();
-    self.cleanPagesTree();
+    self.enableCleanPagesTreeTask();
     self.pushAssets();
     self.addToAdminBar();
     self.apos.pages.addAfterContextMenu(self.menu);
@@ -87,6 +87,11 @@ module.exports = {
     self.composeApiCalls();
     self.addWorkflowModifiedMigration();
     self.addWorkflowLastCommittedMigration();
+
+    if (self.options.autoCommitPageMoves) {
+      self.addPagesTreeCleaningMigration();
+    }
+
     self.on('apostrophe-pages:beforeParkAll', 'updateHistoricalPrefixesPromisified', function() {
       return Promise.promisify(self.updateHistoricalPrefixes)();
     });
